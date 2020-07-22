ae75b01382267481a5090d43e0c98820c0f65167
Lior Bragilevsky <lbragile@sfu.ca>
2020-07-22 14:51:58 -0700

    fixed view for ipad, added exit class to menu and section display on scroll

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var nav_menu = $(".fa-bars");
-var nav_options = nav_menu.next();
+var nav_menu = $(".fa-bars"); // hamburger menu
+var nav_exit = $(".fa-times"); // 'x' mark to exit menu
+var nav_options = nav_exit.next();
 var up_arrow = $(".fa-arrow-up").next();
 var banner_image = $(".banner-img");
 var banner_text = $(".banner-text");
 var text_area = $("textarea");
-var form_buttons = $(".form-buttons");
 
 var main_sections = [".about", ".projects", ".contact"];
 
@@ -13,37 +13,37 @@ const nav_menu_border = nav_menu.css("border");
 // document will be ready if Javascript is placed at end of body
 banner_text.fadeIn(2000, "linear");
 
-// clear the text area to show the placeholder text
-text_area.val("");
+// make sections viewable (appear) only when scrolled to
+$.fn.isInViewport = function () {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
 
-// attempt at making sections viewable (appear) when scrolled to
-// $.fn.isInViewport = function () {
-//   var elementTop = $(this).offset().top;
-//   var elementBottom = elementTop + $(this).outerHeight();
-//   var viewportTop = $(window).scrollTop();
-//   var viewportBottom = viewportTop + $(window).height();
-//   return elementBottom > viewportTop && elementTop < viewportBottom;
-// };
-
-// $(window).on("scroll", function () {
-//   main_sections.forEach((item) => {
-//     if ($(item).isInViewport()) {
-//       $(item).children().show(2000);
-//     }
-//   });
-// });
-
-form_buttons.on("click", function () {
-  text_area.val("");
+$(window).on("scroll", function () {
+  main_sections.forEach((item, index) => {
+    if ($(item).isInViewport() && index != main_sections.length - 1) {
+      $(item).find(":not(h1)").fadeIn(2000);
+    }
+    // don't do this for the form
+    else if (index == main_sections.length - 1) {
+      $(item).find(":not(h1)").show();
+    }
+  });
 });
 
-nav_menu.on("click", function () {
+// toggle the menu if the user clicks on the menu/exit 'buttons'
+nav_menu.add(nav_exit).on("click", function () {
   if (nav_options.css("display") == "none") {
+    nav_menu.css("display", "none");
+    nav_exit.css("visibility", "visible");
     nav_options.show(400);
-    nav_menu.css("border", "none");
   } else {
     nav_options.hide(400);
-    nav_menu.css("border", nav_menu_border);
+    nav_exit.css("visibility", "hidden");
+    nav_menu.show();
   }
 });
 
