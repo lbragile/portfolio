abbb4485844e4c3a1d203dc21de3b40710e03453
Lior Bragilevsky <lbragile@sfu.ca>
2020-07-21 21:52:16 -0700

    layout color improvements and main banner animations

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var nav_menu = $(".fa-bars");
 var nav_options = nav_menu.next();
 var up_arrow = $(".fa-arrow-up").next();
 var banner_image = $(".banner-img");
+var banner_text = $(".banner-text");
 var text_area = $("textarea");
 var form_buttons = $(".form-buttons");
 
@@ -9,18 +10,35 @@ var main_sections = [".about", ".projects", ".contact"];
 
 const nav_menu_border = nav_menu.css("border");
 
-nav_menu.show();
-nav_options.hide();
+// document will be ready if Javascript is placed at end of body
+banner_text.fadeIn(2000, "linear");
 
 // clear the text area to show the placeholder text
 text_area.val("");
+
+// attempt at making sections viewable (appear) when scrolled to
+// $.fn.isInViewport = function () {
+//   var elementTop = $(this).offset().top;
+//   var elementBottom = elementTop + $(this).outerHeight();
+//   var viewportTop = $(window).scrollTop();
+//   var viewportBottom = viewportTop + $(window).height();
+//   return elementBottom > viewportTop && elementTop < viewportBottom;
+// };
+
+// $(window).on("scroll", function () {
+//   main_sections.forEach((item) => {
+//     if ($(item).isInViewport()) {
+//       $(item).children().show(2000);
+//     }
+//   });
+// });
 
 form_buttons.on("click", function () {
   text_area.val("");
 });
 
 nav_menu.on("click", function () {
-  if (nav_options.is(":hidden")) {
+  if (nav_options.css("display") == "none") {
     nav_options.show(400);
     nav_menu.css("border", "none");
   } else {
