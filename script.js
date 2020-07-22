6c91f94efc3fbe07a98d38b3f3724843d4598feb
Lior Bragilevsky <lbragile@sfu.ca>
2020-07-22 15:00:50 -0700

    added functionality to close navigation menu on scroll (extra function), added speed variable

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ var main_sections = [".about", ".projects", ".contact"];
 
 const nav_menu_border = nav_menu.css("border");
 
+var regular_speed = 2000; // 2 seconds
+
 // document will be ready if Javascript is placed at end of body
-banner_text.fadeIn(2000, "linear");
+banner_text.fadeIn(regular_speed, "linear");
 
 // make sections viewable (appear) only when scrolled to
 $.fn.isInViewport = function () {
@@ -22,31 +24,7 @@ $.fn.isInViewport = function () {
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
-$(window).on("scroll", function () {
-  main_sections.forEach((item, index) => {
-    if ($(item).isInViewport() && index != main_sections.length - 1) {
-      $(item).find(":not(h1)").fadeIn(2000);
-    }
-    // don't do this for the form
-    else if (index == main_sections.length - 1) {
-      $(item).find(":not(h1)").show();
-    }
-  });
-});
-
-// toggle the menu if the user clicks on the menu/exit 'buttons'
-nav_menu.add(nav_exit).on("click", function () {
-  if (nav_options.css("display") == "none") {
-    nav_menu.css("display", "none");
-    nav_exit.css("visibility", "visible");
-    nav_options.show(400);
-  } else {
-    nav_options.hide(400);
-    nav_exit.css("visibility", "hidden");
-    nav_menu.show();
-  }
-});
-
+// scroll to an element
 $.fn.scrollToThis = function (speed) {
   $("html, body").animate(
     {
@@ -56,25 +34,61 @@ $.fn.scrollToThis = function (speed) {
   );
 };
 
+function hideNavigationOptions() {
+  nav_options.hide(regular_speed / 4);
+  nav_exit.css("visibility", "hidden");
+  nav_menu.show();
+}
+
+$(window).on("scroll", function () {
+  main_sections.forEach((item, index) => {
+    if ($(item).isInViewport() && index != main_sections.length - 1) {
+      $(item).find(":not(h1)").fadeIn(regular_speed);
+    }
+    // don't do this for the form
+    else if (index == main_sections.length - 1) {
+      $(item).find(":not(h1)").show();
+    }
+  });
+
+  // if user scrolls with the navigation menu open, it should close
+  hideNavigationOptions();
+});
+
+// toggle the menu if the user clicks on the menu/exit 'buttons'
+nav_menu.add(nav_exit).on("click", function () {
+  if (nav_options.css("display") == "none") {
+    nav_menu.css("display", "none");
+    nav_exit.css("visibility", "visible");
+    nav_options.show(regular_speed / 4);
+  } else {
+    hideNavigationOptions();
+  }
+});
+
 main_sections.forEach((item, index) => {
   nav_options
     .first()
     .children()
     .eq(index)
     .on("click", function () {
-      $(item).scrollToThis(1000);
+      $(item).scrollToThis(regular_speed / 2);
     });
 });
 
 // arrow effects
 up_arrow.on("mouseenter", function () {
-  $(this).prev().animate({ height: "+=10px" }, 400);
+  $(this)
+    .prev()
+    .animate({ height: "+=10px" }, regular_speed / 4);
 });
 
 up_arrow.on("mouseleave", function () {
-  $(this).prev().animate({ height: "-=10px" }, 400);
+  $(this)
+    .prev()
+    .animate({ height: "-=10px" }, regular_speed / 4);
 });
 
 up_arrow.on("click", function () {
-  banner_image.scrollToThis(2000);
+  banner_image.scrollToThis(regular_speed);
 });
